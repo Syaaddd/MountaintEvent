c96fc67e60c8ddb229993ead6630ec37ccabc5e5
Sy4dd <105086466+Arsyad2009@users.noreply.github.com>
2025-08-18 17:32:47 +0700

    first commit

---
+++ src/main/java/com/mountaintevent/Mountaintevent.java
@@ -0,0 +1,17 @@
+package com.mountaintevent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mountaintevent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
